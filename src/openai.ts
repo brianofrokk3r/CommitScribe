b808f7f04f362fe508352be45c66cbc0af9be3f2
Brian Sanchez <brian@rokk3rlabs.com>
2023-03-18 13:00:31 -0400

    📝 docs(openai.ts): update prompt to reflect audience change from engineering team to product team

--- src/openai.ts
+++ src/openai.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { OPENAI_API_KEY } from './config';
 
 export async function generateCommitSummary(log: string) {
-  let fullPrompt = `You are a helpful assistant. Your audience is the engineering team. Give me a set of sub-lists broken into feature, fix, chore, refactor, etc... based on the commit logs provided. Append after each Section title the % of items in that section. If there's nothing in a section don't put it in the result. Remove words like fix, style, chore, refactor from the front of the items, and just put a section head and group the items in each section.
+  let fullPrompt = `You are a helpful assistant. Your audience is the product team. Give me a set of sub-lists broken into feature, fix, chore, refactor, etc... based on the commit logs provided. Append after each Section title the % of items in that section. If there's nothing in a section don't put it in the result. Remove words like fix, style, chore, refactor from the front of the items, and just put a section head and group the items in each section.
     
 The format I would like is...
 
