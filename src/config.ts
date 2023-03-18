7936201514d9310e69caeb398292c8d279eb20c9
Brian Sanchez <brian@rokk3rlabs.com>
2023-03-18 09:58:55 -0400

    🆕 feat(package.json): add build, start, and dev dependencies 🆕 feat(config.ts): add OPENAI_API_KEY environment variable and config file 🆕 feat(git.ts): add functions to get git logs and last merge
    ✨ feat(index.ts): add command-line interface using commander.js to generate user-friendly updates from commits
    
    ✨ feat(openai.ts): add function to generate commit summary using OpenAI API
    ✨ feat(utils.ts): add function to get Node.js version from package.json
    
    🎉 feat(tsconfig.json): add TypeScript configuration file with strict type-checking options and commonjs module code generation

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '.getconfig' });
+
+if (!process.env.OPENAI_API_KEY) {
+  console.error('No OpenAI API Key found. Please run `get config -k <key>` to set your API key.');
+  process.exit(1);
+}
+
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+export function getNodeVersion() {
+    const packageJson = fs.readFileSync('./package.json', 'utf-8');
+    const { version } = JSON.parse(packageJson);
+    return version;
+}
