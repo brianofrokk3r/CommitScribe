112a94150eeab067ff3921756d2298b548f1f66f
Brian Sanchez <brian@rokk3rlabs.com>
2023-03-19 13:34:23 -0400

    🐛 fix(openai.ts): declare messages array as an array of objects ✨ feat(openai.ts): split prompt into system and user messages for weekly and between prompt types

--- src/openai.ts
+++ src/openai.ts
@@ -3,7 +3,8 @@ import { OPENAI_API_KEY } from './config';
 
 export async function generateCommitSummary(log: string, promptType: string = 'weekly') {
 
-  let fullPrompt = ``;
+  let fullPrompt: string = ``;
+  let messages: Array<object> = [];
 
   switch (promptType) {
     case 'weekly':
@@ -13,7 +14,10 @@ The format I would like is...
 emoji Feature (20%):
 - Feature 1 description
 - Feature 2 description
-- etc...` + '\n\n-----\n\n' + log;
+- etc...`;
+
+      messages.push({ role: 'system', content: fullPrompt })
+      messages.push({ role: 'user', content: '\n\n-----\n\n' + log })
       break;
     case 'between':
       fullPrompt = `You are a helpful assistant. Your audience is the product team. You are tasked to generate a summary based on the content provided.
@@ -24,23 +28,23 @@ Here are the guidelines to follow.
 - If there is nothing in a section don't put it in the result. 
 - If there is nothing to summarize, do not respond with anything.
 - If there is a version number, do not try to make up version details from your own knowledge.
-- Ignore versions, just focus on the logs.
-  
-Here are the logs:
-  
-${log}`;
+- Ignore versions, just focus on the logs.`;
+
+      let logPrompt = `
+      Here are the logs:
+      ${log}`;
+      messages.push({ role: 'system', content: fullPrompt })
+      messages.push({ role: 'user', content: logPrompt })
       break;
     default:
       fullPrompt = `You are a helpful assistant. Your audience is the product team. You are tasked to generate a summary based on the content provided.`;
   }
 
-  console.log(promptType, fullPrompt);
-
   const completion = await axios.post(
     'https://api.openai.com/v1/chat/completions',
     {
       model: 'gpt-3.5-turbo-0301',
-      messages: [{ role: 'user', content: fullPrompt }],
+      messages: messages,
       temperature: 0,
       presence_penalty: 2,
     },
